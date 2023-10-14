2d21cdee35d71d84d5c17bd847d7a41543729cdc
Just-focus <cxgnicerr@gmail.com>
2023-10-14 16:53:45 +0800

    修改

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import App from './app';
-import './styles/reset.scss';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
 );
